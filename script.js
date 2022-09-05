a61014c5043aaa321506f698b0c7e649126c6f85
Angela Kim <aangelakkim@gmail.com>
2022-09-04 21:02:21 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var css = document.querySelector('h3');
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var body = document.getElementById('gradient');
+var button = document.getElementById('random');
+
+function setGradient() {
+    body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
+
+    css.textContent = body.style.background + ';';
+}
+
+function random() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    const randomColor1 = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color1.value = "#" + randomColor;
+    color2.value = "#" + randomColor1;
+}
+button.addEventListener("click", () => { random(); setGradient(); });
+
+
+color1.addEventListener('input', setGradient)
+
+color2.addEventListener('input', setGradient)
